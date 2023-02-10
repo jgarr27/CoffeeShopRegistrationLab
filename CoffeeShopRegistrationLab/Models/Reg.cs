ee66227f60897bf07da501c64b5c821df6f7f862
jgarr27 <117048949+jgarr27@users.noreply.github.com>
2023-02-10 08:15:49 -0500

    first

---
+++ CoffeeShopRegistrationLab/Models/Reg.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopRegistrationLab.Models
+{
+    public class Reg
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        private string Password { get; set; }
+    }
+}
